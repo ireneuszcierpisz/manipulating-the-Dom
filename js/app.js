252610fb9b3bb5e845ba48d157002a3ced922375
ireneuszcierpisz <>
2023-11-22 23:38:32 +0000

    Add examples of script for manipulating the DOM

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 console.log('Hello World');
+
+// MANIPULATING the DOM;
+// var article = document.getElementById('home-page');
+// var section = document.getElementsByTagName('section');
+// var leads = document.getElementsByClassName('lead');
+// sections = document.querySelector('section');
+// sections = document.querySelectorAll('section');
+// var lead = document.querySelector('.lead');
+// lead.textContent;
+// article.children;
+// article.parent;
+// article.nextElementSibling;
+// var section = article.firstElementChild;
+// section.nextElementSibling;
+// var li = document.createElement('li');
+// li.classList.add('last');
+// li.style.backgroundColor('pink');
+// var a = document.createElement('a');
+// a.textContent = 'four';
+// a.style.color = 'white';
+// li.appendChild(a);
+// var uls = document.getElementsByTagName('ul');
+// var ul = uls[0];
+// ul.appendChild(li);
+// var ul = document.getElementsByTagName('ul');
+// var lis = ul[0].getElementsByTagName('li');
+// var firstLi = lis[0];
+// var lastLi = lis[3];
+// firstLi.classList.remove('active');
+// ul[0].insertBefore(lastLi, firstLi);
+// for (var i = 0; i < lis.length; i++) { lis[i].style.backgroundColor = 'pink'; }
+// ul.find('li').removeClass('active').addClass('inactive').css('background-color', 'pink');  // ??? Uncaught TypeError: ul.find is not a function
